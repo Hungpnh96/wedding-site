1808562d5e003fe6dd7adcaf075ba409c48f75fc
Phạm Ngọc Huy Hùng <hungpnh96@gmail.com>
2024-11-03 12:46:04 +0700

    Commit new version send message

--- js/myjs.js
+++ js/myjs.js
@@ -99,3 +99,65 @@ document.addEventListener('DOMContentLoaded', function () {
 		audioIcon.classList.add('fa-volume-mute');
 	});
 });
+
+
+document.getElementById('SendMessage').addEventListener('click', function () {
+    debugger;
+    // Lấy giá trị từ các ô dữ liệu
+    const senderName = document.getElementById('senderName').value;
+    const from = document.getElementById('from').value;
+    const content = document.getElementById('message').value;
+
+    // Tạo đối tượng dữ liệu JSON
+    const data = {
+        SenderName: senderName,
+        From: from,
+        Content: content
+    };
+
+    // Hiển thị loading spinner
+    document.getElementById('loading').style.display = 'block';
+
+    fetch('http://hungpnh.myftp.org/api/v1/telegram/sendMessage', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', // Đặt Content-Type là application/json
+        },
+        body: JSON.stringify(data) // Chuyển đổi đối tượng data thành chuỗi JSON
+    })
+    .then(response => {
+        // Ẩn loading spinner
+        document.getElementById('loading').style.display = 'none';
+        
+        return response.json(); // Chuyển đổi phản hồi thành JSON
+    })
+    .then(data => {
+        // Kiểm tra mã trạng thái code trong phản hồi
+        if (data.code === 200) {
+            // Nếu code là 200, hiển thị thông báo thành công
+            Swal.fire({
+                icon: 'success',
+                title: 'Thành công!',
+                text: data.message // Hiển thị message từ phản hồi
+            });
+        } else {
+            // Nếu code không phải 200, hiển thị thông báo thất bại
+            Swal.fire({
+                icon: 'error',
+                title: 'Thất bại!',
+                text: data.message // Hiển thị message từ phản hồi
+            });
+        }
+    })
+    .catch(error => {
+        // Ẩn loading spinner khi có lỗi
+        document.getElementById('loading').style.display = 'none';
+        
+        // Hiển thị thông báo lỗi hệ thống
+        Swal.fire({
+            icon: 'error',
+            title: 'Lỗi!',
+            text: `Đã xảy ra lỗi: ${error.message}. Vui lòng thử lại sau.`
+        });
+    });
+});
